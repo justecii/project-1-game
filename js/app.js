910fc026859813e7e19d33e3ce3caf0c81262f59
justecii <justecii@gmail.com>
2017-09-12 00:24:13 -0700

    changed to star wars slot

--- js/app.js
+++ js/app.js
@@ -1,15 +1,62 @@
 var startingCredits = 0;
 var startingBet = 0;
-var totalCredits = document.getElementById("totalCredits")
-var betSize = document.getElementById("activeBet")
-var betOne = document.getElementById('button1');
-var betThree = document.getElementById('button3');
-var betFive = document.getElementById('button5');
-var betTwenty = document.getElementById('button20');
-var betSixty = document.getElementById('button60');
-var betHundo = document.getElementById('button100');
+var totalCredits = document.getElementById("totalCredits");
+var betSize = document.getElementById("activeBet");
+var lever =document.getElementById("handle");
+// var betOne = document.getElementById('button1');
+// var betThree = document.getElementById('button3');
+// var betFive = document.getElementById('button5');
+// var betTwenty = document.getElementById('button20');
+// var betSixty = document.getElementById('button60');
+// var betHundo = document.getElementById('button100');
+var slotImages = [
+	{
+		name: "kylo",
+		source: "images/kylo.png"
 
+	},
+	{
+		name: "leia",
+		source: "images/leia.png"
+	},
+	{
+		name: "x-wing",
+		source: "images/x-wing.gif"
+	},
+	{
+		name: "chewy",
+		source: "images/chewy.png"
+	},
+	{
+		name: "solo",
+		source: "images/solo.gif-c200"
+	},
+	{
+		name: "droid",
+		source: "images/droid.gif"
+	},
+	{
+		name: "rebel",
+		source: "images/rebel.png"
+	},
+	{
+		name: "empire",
+		source: "images/empire.png"
+	},
+	{
+		name: "rey",
+		source: "images/rey.png"
+	},
+	{
+		name: "vader",
+		source:"images/vader.gif"
+	}
 
+];
+var link = document.createElement("img");
+  link.src = slotImages[Math.floor(Math.random()*10)].source;
+$('.row1').append(link);
+console.log(link.src);
 
 $('#button1').on('click', function(){
 	startingBet = 1;
@@ -53,3 +100,8 @@ function betWithZeros(number, length) {
     return my_string;
 };
 betSize.textContent = betWithZeros(startingBet, 3);
+
+// move the handle on click
+$('#handle').on('click', function(){
+	lever.style.transform = "rotate(45deg)";
+})
